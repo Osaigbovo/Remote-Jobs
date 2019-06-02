3a9acfbe5940feae82906d9600ef319b35fee323
Osaigbovo <odiase.osaigbovo@gmail.com>
2019-06-02 15:46:47 +0100

    install release and keystore

--- app/src/main/java/io/github/osaigbovo/remotejobs/ui/about/AboutActivity.java
+++ app/src/main/java/io/github/osaigbovo/remotejobs/ui/about/AboutActivity.java
@@ -4,13 +4,14 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import butterknife.BindArray;
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.github.osaigbovo.remotejobs.R;
@@ -19,6 +20,10 @@ public class AboutActivity extends AppCompatActivity implements View.OnClickList
 
     @BindView(R.id.about_toolbar)
     Toolbar toolbar;
+    @BindArray(R.array.library_link)
+    String[] library_link;
+    @BindString(R.string.error)
+    String error;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +38,7 @@ public class AboutActivity extends AppCompatActivity implements View.OnClickList
             toolbar.setNavigationOnClickListener(view -> onBackPressed());
         }
 
-        TextView[] containers = new TextView[]{
+        TextView[] textViews = new TextView[]{
                 findViewById(R.id.text_retrofit_desc),
                 findViewById(R.id.text_glide_desc),
                 findViewById(R.id.text_androidx_desc),
@@ -47,8 +52,8 @@ public class AboutActivity extends AppCompatActivity implements View.OnClickList
                 findViewById(R.id.text_timber_desc),
                 findViewById(R.id.text_text_drawable_desc),
                 findViewById(R.id.text_gson_desc)};
-        for (TextView r : containers) {
-            r.setOnClickListener(this);
+        for (TextView textView : textViews) {
+            textView.setOnClickListener(this);
         }
 
     }
@@ -57,56 +62,44 @@ public class AboutActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.text_retrofit_desc:
-                goToURL("https://github.com/square/retrofit");
+                goToURL(library_link[0]);
                 break;
-
-//            case R.id.container_about_library2:
-//                goToURL("https://github.com/bumptech/glide");
-//                break;
-//
-//            case R.id.container_about_library3:
-//                goToURL("https://developer.android.com/jetpack/androidx");
-//                break;
-//
-//            case R.id.container_about_library4:
-//                goToURL("https://github.com/airbnb/lottie-android");
-//                break;
-//
-//            case R.id.container_about_library5:
-//                goToURL("https://developer.android.com/topic/libraries/architecture/room");
-//                break;
-//
-//            case R.id.container_about_library6:
-//                goToURL("https://github.com/ReactiveX/RxJava");
-//                break;
-//
-//            case R.id.container_about_library7:
-//                goToURL("https://github.com/JakeWharton/butterknife");
-//                break;
-//
-//            case R.id.container_about_library9:
-//                goToURL("https://github.com/google/dagger");
-//                break;
-//
-//            case R.id.container_about_library10:
-//                goToURL("https://github.com/google/gson");
-//                break;
-//
-//            case R.id.container_about_library11:
-//                goToURL("https://github.com/facebook/stetho");
-//                break;
-//
-//            case R.id.container_about_library12:
-//                goToURL("https://github.com/square/leakcanary");
-//                break;
-//
-//            case R.id.container_about_library13:
-//                goToURL("https://github.com/JakeWharton/timber");
-//                break;
-//
-//            case R.id.container_about_library14:
-//                goToURL("https://github.com/amulyakhare/TextDrawable");
-//                break;
+            case R.id.text_glide_desc:
+                goToURL(library_link[1]);
+                break;
+            case R.id.text_androidx_desc:
+                goToURL(library_link[2]);
+                break;
+            case R.id.text_lottie_desc:
+                goToURL(library_link[3]);
+                break;
+            case R.id.text_room_desc:
+                goToURL(library_link[4]);
+                break;
+            case R.id.text_rxjava_desc:
+                goToURL(library_link[5]);
+                break;
+            case R.id.text_butter_desc:
+                goToURL(library_link[6]);
+                break;
+            case R.id.text_dagger_desc:
+                goToURL(library_link[7]);
+                break;
+            case R.id.text_gson_desc:
+                goToURL(library_link[8]);
+                break;
+            case R.id.text_stetho_desc:
+                goToURL(library_link[9]);
+                break;
+            case R.id.text_leak_desc:
+                goToURL(library_link[10]);
+                break;
+            case R.id.text_timber_desc:
+                goToURL(library_link[11]);
+                break;
+            case R.id.text_text_drawable_desc:
+                goToURL(library_link[12]);
+                break;
         }
     }
 
@@ -116,7 +109,7 @@ public class AboutActivity extends AppCompatActivity implements View.OnClickList
         if (intent.resolveActivity(getPackageManager()) != null)
             startActivity(intent);
         else
-            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
     }
 
 }
